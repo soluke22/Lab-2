a81cf15bb446ecd53328c7b2f9d18621d6c65b4f
soluke22 <soluke22@gmail.com>
2019-11-12 22:01:43 -0500

    added description to project 2 and start the input lines

---
+++ project.s
@@ -0,0 +1,19 @@
+# N = 35, '0' to '9', 'a' to 'y', 'A' to 'Y'
+# M = 35-10 = 25
+#Steps:
+#1)Take in a string of up to 1000 characters
+#2)Remove leading and trailing blank spaces "  "<-this->"  "
+#3)If the string is: a) 0 characters b) greater than 4 characters or c)has one illegal character
+#4)If the string contains allowed characters, convert the charaacter to its decimal equivalent i.e. a = 10
+
+.data 
+
+ask:
+	.asciiz "\Input: \n"
+
+input:
+	.space 1000
+
+.text
+
+main:
